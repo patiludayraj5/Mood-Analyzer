ea64bbd08c0559e44a7630856100ffd8d46db4fa
Uday <uday@gmail.com>
2024-01-02 09:28:07 +0530

    Added test cases

--- test/java/TestMood_Analyzer.java
+++ test/java/TestMood_Analyzer.java
@@ -1,3 +1,20 @@
-public class TestMood_Analyzer {
+import org.example.Mood_Analyzer;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestMood_Analyzer {
+    @Test
+    public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+        Mood_Analyzer moodAnalyzer = new Mood_Analyzer("I am in Sad Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        Mood_Analyzer moodAnalyzer = new Mood_Analyzer("I am in Any Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
